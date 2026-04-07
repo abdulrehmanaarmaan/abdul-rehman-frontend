6e2e2d720733606ccd0eda5c8a437a1b52af8cc2
AbdulRehmanAarmaan <abdulrehmanaarmaan@gmail.com>
2026-04-07 13:17:15 +0600

    build course-modules section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,7 +12,7 @@ export default function Home() {
       <HeroSection />
       <FocusSupportSection />
       <CourseTimelineSection />
-      {/* <CourseModules /> */}
+      <CourseModules />
       {/* <TestimonialsSection /> */}
       {/* <Pricing /> */}
     </div>
