688a049484fba4750630150c535a57d8aebdcdcb
AbdulRehmanAarmaan <abdulrehmanaarmaan@gmail.com>
2026-04-07 13:19:55 +0600

    build testimonials section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,7 +13,7 @@ export default function Home() {
       <FocusSupportSection />
       <CourseTimelineSection />
       <CourseModules />
-      {/* <TestimonialsSection /> */}
+      <TestimonialsSection />
       {/* <Pricing /> */}
     </div>
   );
